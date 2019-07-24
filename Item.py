4e831490b607bca5d878063458c0b7ed498f3f7b
DarkRaider95 <tommy061295@gmail.com>
2019-07-24 11:33:19 +0200

    Aggiunti i rettangoli liberi

---
+++ Item.py
@@ -0,0 +1,49 @@
+"""
+2D Item class.
+"""
+class Item:
+    """
+    Items class for rectangles inserted into sheets
+    """
+    def __init__(self, width, height, x=0, y=0, rotated=False) -> None:
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.area = self.width * self.height
+        self.rotated = rotated
+        self.id = 0
+
+
+    def __repr__(self):
+        return 'Item(width=%r, height=%r, x=%r, y=%r)' % (self.width, self.height, self.x, self.y)
+
+
+    def rotate(self) -> None:
+        self.width, self.height = self.height, self.width
+        self.rotated = False if self.rotated == True else True
+
+"""
+2D Item class.
+"""
+class Container:
+    """
+    Items class for rectangles inserted into sheets
+    """
+    def __init__(self, width, height, x=0, y=0, rotated=False) -> None:
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.area = self.width * self.height
+        self.rotated = rotated
+        self.id = 0
+
+
+    def __repr__(self):
+        return 'Item(width=%r, height=%r, x=%r, y=%r)' % (self.width, self.height, self.x, self.y)
+
+
+    def rotate(self) -> None:
+        self.width, self.height = self.height, self.width
+        self.rotated = False if self.rotated == True else True
--- RectanglePackingProblem.py
+++ RectanglePackingProblem.py
@@ -4,77 +4,137 @@ from matplotlib.collections import PatchCollection
 import random
 from Rect import Rect
 import math
+from Guillotine import Guillotine
+from Guillotine import FreeRectangle
+from Item import Item
+from Shelf import Shelf
 
-def drawRect(bin, rectangles):
+def drawRect(bin, rectangles, wastemap, shelves):
 
-	patches = []
-	patches.append(Rectangle((bin.x,bin.y),bin.w,bin.h))
+	#print(rectangles)
+
+	patches1 = []
+	patches2 = []
+	patches3 = []
+
+	patches1.append(Rectangle((bin.x,bin.y),bin.width,bin.height))
 
 	for r in rectangles:
-		patches.append(Rectangle((r.x,r.y),r.w,r.h))
+		patches2.append(Rectangle((r.x,r.y),r.width,r.height))
+
+	for r in wastemap.freerects:
+		patches3.append(Rectangle((r.x,r.y),r.width,r.height))
 	
-	pc = PatchCollection(patches, facecolor='None', alpha=1, edgecolor='r')
+	pc1 = PatchCollection(patches1, facecolor='None', alpha=1, edgecolor='b')
+	pc2 = PatchCollection(patches2, facecolor='None', alpha=1, edgecolor='r')
+	pc3 = PatchCollection(patches3, facecolor='None', alpha=1, edgecolor='g')
 	
 	fig, ax = plt.subplots(1)
 	
-	ax.add_collection(pc)
+	ax.add_collection(pc1)
+	ax.add_collection(pc2)
+	ax.add_collection(pc3)
 	
 	#imposta coordinate visuale piano cartesiano
-	ax.set_xlim((-10,bin.w+100),auto=True)
-	ax.set_ylim((-10,bin.h+100),auto=True)
+	ax.set_xlim((-100,bin.width+100),auto=True)
+	ax.set_ylim((-100,bin.height+100),auto=True)
 	
 	plt.show()
 
-def greedyShelf(bin, rectangles):
+def greedyShelf(bin, shelves, rectangles, wastemap):
 	rect_inserted = []
-	rectangles.sort(key=lambda x: (x.h,x.w), reverse=True)#(x.h,x.w), reverse=True)
+	rectangles.sort(key=lambda x: (x.height,x.width), reverse=True)#(x.h,x.w), reverse=True)
 
-	sheight=rectangles[0].h
+	sheight=rectangles[0].height
 
 	hlimit=False
 
-	y=0
+	y=bin.height
 	i=0
 
-	while(not hlimit):
-		x=0
-		while(i < len(rectangles) and x+rectangles[i].w<bin.w):
-			#rectangles.pop(0)
-			rectangles[i].x=x
-			rectangles[i].y=y
-			rect_inserted.append(rectangles[i])
+	if(sheight <= bin.height):
+		while(not hlimit):			
+			#x=bin.w
+			#pare non mi debba preoccupare di available height del bin
+			sh=Shelf(bin.width, sheight, bin.height-y)
+			shelves.append(sh)
+			y-=sheight
 
-			x+=rectangles[i].w
-			i+=1
-
-		if i >= len(rectangles):
-			hlimit=True
-		else:
-			# sale di scaffale
-			w_rimanente = bin.w - x
-			y_rimanente = sheight
-
-			y+=sheight
-			sheight=rectangles[i].h
-			
-			if y+sheight > bin.h:
+			#while(i < len(rectangles) and x+rectangles[i].width<bin.width):						
+			while(i < len(rectangles) and sh._item_fits_shelf(rectangles[i])):
+				#print("entrato while while")
+				#rectangles.pop(0)
+				#rectangles[i].x=x
+				#rectangles[i].y=y
+				#Inseriamo nello scaffale
+				sh.insert(rectangles[i])
+				rect_inserted.append(rectangles[i])
+	
+				#x+=rectangles[i].width
+				i+=1
+	
+			if i >= len(rectangles):
 				hlimit=True
+			else:
+				# sale di scaffale
+				#w_rimanente = bin.width - x
+				#y_rimanente = sheight
+	
+				#y+=sheight
+				sheight=rectangles[i].height
+				
+				if y - sheight <= 0:
+					hlimit=True				
+					
+				#if y+sheight > bin.height:
+				#	hlimit=True
+				#else:
+				#	_add_to_wastemap(bin,sh,wastemap)
+			_add_to_wastemap(bin,sh,wastemap)
 
-	return rect_inserted
+	return rect_inserted,wastemap,shelves
+
+def _add_to_wastemap(bin,shelf,wastemap):
+		""" Add lost space above items to the wastemap """
+		# Add space above items to wastemap
+		for item in shelf.items:
+			if item.height < shelf.y:
+				freeWidth = item.width
+				freeHeight = shelf.y - item.height
+				freeX = item.x
+				freeY = item.height + shelf.vertical_offset
+				freeRect = FreeRectangle(freeWidth,	freeHeight, freeX, freeY)
+				wastemap.freerects.add(freeRect)
+		# Move remaining shelf width to wastemap
+		if shelf.available_width > 0:
+			freeWidth = shelf.available_width
+			freeHeight = shelf.y
+			freeX = bin.width - shelf.available_width
+			freeY = shelf.vertical_offset
+			freeRect = FreeRectangle(freeWidth, freeHeight, freeX, freeY)
+			wastemap.freerects.add(freeRect)
+		# Close Shelf
+		shelf.available_width = 0
+		# Merge rectangles in wastemap
+		wastemap.rectangle_merge()
 
 def main():
-	bin=Rect(0,0,500,500)
+	bin=Item(500,500,0,0)
 
 	rectangles=[]
+	shelves = []
 	
-	for i in range(1000):
+	for i in range(100):
 		w = random.randint(10, 50)
 		h = random.randint(50, 100)
-		rectangles.append(Rect(0,0,h,w))
+		rectangles.append(Item(h,w))
+		#rectangles.append(Rect(0,0,h,w))
 
-	rect_inserted = greedyShelf(bin, rectangles)
+	wastemap = Guillotine(0, 0, rotation = False, heuristic='best_area')
 
-	drawRect(bin, rect_inserted)
+	rect_inserted,wastemap,shelves = greedyShelf(bin, shelves, rectangles, wastemap)
+
+	drawRect(bin, rect_inserted,wastemap,shelves)
 
 if __name__ == '__main__':
 		main()
